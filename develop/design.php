1cdd399939dd7a94fd9044606e7a34a9ade319ea
福井彩佳 <ayaka.fsp@gmail.com>
2025-05-27 16:09:21 +0900

    no message

--- develop/design.php
+++ develop/design.php
@@ -9,7 +9,7 @@
   "keywords" => "デザイン,",
   "description" => "デザインページ。",
   "add_stylesheet" => ["css/design.css"],
-  "add_script" =>  [""],
+  "add_script" =>  ["js/design.js"],
   "canonical_page_id" => "design", 
   "logo_text" => "デザイン｜"
 );
@@ -28,8 +28,10 @@
 	<!--// パンくずリスト -->
 
   <header class="main-visual clearfix">
-   <div class="image"><img src="./images/design/mv.jpg" alt="" class="">
-   <p class="annotation white">外観完成予想図</p></div>
+   <div class="top-mv__slide">
+    <figure><img src="./images/design/mv01.jpg" alt="" class="w_100"><p class="annotation white">外観完成予想図(1)</p></figure>
+    <figure><img src="./images/design/mv02.jpg" alt="" class="w_100"><p class="annotation white">外観完成予想図(2)</p></figure>
+  </div><!--//top-mv -->
   </header><!-- main-visual -->
 
   
@@ -43,7 +45,7 @@
 
 
 <div class="annotation-area">
-<p class="page-annotation js-scroll wrap clearfix">※掲載の外観完成予想図は計画段階の図面を基に描き起こしたもので、実際とは多少異なります。また、形状の細部および設備機器等については省略しております。<br>※敷地内の樹木等はある程度生育した後をイメージして描いたものです。また、葉や花の色合い、樹形等はイメージであり実際とは異なります。なお、植栽計画は変更される場合がありますので、あらかじめご了承ください。</p></div>
+<p class="page-annotation js-scroll wrap clearfix">※掲載の外観完成予想図(1)は計画段階の図面を基に描き起こしたもので、実際とは多少異なります。また、形状の細部および設備機器等については省略しております。<br>※敷地内の樹木等はある程度生育した後をイメージして描いたものです。また、葉や花の色合い、樹形等はイメージであり実際とは異なります。なお、植栽計画は変更される場合がありますので、あらかじめご了承ください。<br>※掲載の外観完成予想図(2)は庚申堂公園から西方向を撮影した写真（2024年12月撮影）に計画段階の図面を基に描き起こした建物完成予想図をＣＧ合成・加工したもので、実際とは異なります。また、周辺環境・眺望は将来変わる場合があります。</p></div>
 </main>
 
 <?php require(INCLUDES."footer.php"); ?>
